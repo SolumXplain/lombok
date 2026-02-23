d807261313b7ee82bbee058546ecd1f4c2f954d9
Neale Upstone <neale@nealeupstone.com>
2026-02-23 22:07:12 +0000

    Use TypeLibrary for matching annotations in HandlerUtil classes

--- src/core/lombok/javac/handlers/JavacHandlerUtil.java
+++ src/core/lombok/javac/handlers/JavacHandlerUtil.java
@@ -1738,11 +1738,12 @@ public class JavacHandlerUtil {
 	}
 
 	public static boolean hasNonNullAnnotations(JavacNode node) {
+		TypeResolver resolver = node.getImportListAsTypeResolver();
 		for (JavacNode child : node.down()) {
 			if (child.getKind() == Kind.ANNOTATION) {
 				JCAnnotation annotation = (JCAnnotation) child.get();
 				String annotationTypeName = getTypeName(annotation.annotationType);
-				for (String nn : NONNULL_ANNOTATIONS) if (typeMatches(nn, node, annotationTypeName)) return true;
+				if (resolver.typeRefToFullyQualifiedName(node, NONNULL_TYPE_LIBRARY, annotationTypeName) != null) return true;
 			}
 		}
 
@@ -1751,9 +1752,10 @@ public class JavacHandlerUtil {
 
 	public static boolean hasNonNullAnnotations(JavacNode node, List<JCAnnotation> anns) {
 		if (anns == null) return false;
+		TypeResolver resolver = node.getImportListAsTypeResolver();
 		for (JCAnnotation ann : anns) {
 			String annotationTypeName = getTypeName(ann.annotationType);
-			for (String nn : NONNULL_ANNOTATIONS) if (typeMatches(nn, node, annotationTypeName)) return true;
+			if (resolver.typeRefToFullyQualifiedName(node, NONNULL_TYPE_LIBRARY, annotationTypeName) != null) return true;
 		}
 
 		return false;
